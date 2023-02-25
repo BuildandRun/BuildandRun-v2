cfa36fa6b5d728ab1906e7fe7ef21a3e4cd8f03c
AmonKissi <amondkissi@gmail.com>
2023-02-25 00:46:17 -0600

    pricing fixed

--- my-app/apps/app/.next/static/chunks/webpack.js
+++ my-app/apps/app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c310c5e4e6bd7047"; }
+/******/ 		__webpack_require__.h = function() { return "74bd28a2b4908c28"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
