2fcb54dc41713f0f622dcd0d33507b0fa5ae1608
AmonKissi <amondkissi@gmail.com>
2023-02-24 22:29:39 -0600

    fixed terminal

--- my-app/apps/app/.next/static/chunks/webpack.js
+++ my-app/apps/app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ce44cb8f4b09b32c"; }
+/******/ 		__webpack_require__.h = function() { return "6c485cad3a311d2a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
