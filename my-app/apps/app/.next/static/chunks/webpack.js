658cfd9d09ad672986840ef3182040b1d1f20fb3
Your Name <amondkissi@gmail.com>
2023-03-08 19:49:51 -0600

    fixed debit

--- my-app/apps/app/.next/static/chunks/webpack.js
+++ my-app/apps/app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9620b2cc248c43e8"; }
+/******/ 		__webpack_require__.h = function() { return "7836950793384105"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
