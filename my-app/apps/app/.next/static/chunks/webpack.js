277a6f9d70ccc2a40ae56ba8d0280f9402cd3747
Your Name <amondkissi@gmail.com>
2023-03-08 21:24:39 -0600

    fixed image

--- my-app/apps/app/.next/static/chunks/webpack.js
+++ my-app/apps/app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7836950793384105"; }
+/******/ 		__webpack_require__.h = function() { return "59692eb53d03368b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
