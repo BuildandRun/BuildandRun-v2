2b728c8cb85fe01fe26f1bb7112ae8063e862713
Your Name <amondkissi@gmail.com>
2023-05-16 16:56:33 -0500

    change redirect to 30 seconds

--- my-app/apps/app/.next/static/chunks/webpack.js
+++ my-app/apps/app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "118625ff809264b7"; }
+/******/ 		__webpack_require__.h = function() { return "e672da5ccfdb7a0d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
