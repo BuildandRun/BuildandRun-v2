b3736fd3ac4b25a8b604e295d1e26afa13ec43ea
Your Name <amondkissi@gmail.com>
2024-03-01 11:51:01 -0600

    progressing in pricing

--- my-app/apps/app/.next/static/chunks/webpack.js
+++ my-app/apps/app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1e316905ea496f45"; }
+/******/ 		__webpack_require__.h = function() { return "34e54b0d36e25bf3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- my-app/apps/app/pages/plan.tsx
+++ my-app/apps/app/pages/plan.tsx
@@ -1,34 +1,65 @@
-import React from 'react';
+import { features } from "process";
+import React from "react";
 
 interface PlanProps {
   name: string;
   price: string;
   description: string;
+  features: string;
 }
 
-const Plan: React.FC<PlanProps> = ({ name, price, description }) => {
+const Plan: React.FC<PlanProps> = ({ name, price, description, features }) => {
   return (
-    <div className="plan">
-      <h2>{name}</h2>
-      <p className="price">{price}</p>
-      <p>{description}</p>
+    <div className="plan cardplan">
+      <h3>{name}</h3>
+      <p className="plan-price">{price}</p>
+      <p className="plan-description">{description}</p>
+      <p className="plan-features">{features}</p>
       <button>Get started</button>
     </div>
   );
-}
+};
 
 const ChoosePlan: React.FC = () => {
   return (
     <div className="plan-container">
-        <div className='plan-house'>
-      <h1>Choose Your Plan</h1>
-      <div className="plans">
-        <Plan name="Regular" price="$9.99/month" description="Access to basic features" />
-        <Plan name="Premium" price="$19.99/month" description="Access to premium features" />
-        <Plan name="Unlimited Access" price="$29.99/month" description="Access to all features with no limits" />
-      </div></div>
+      <div className="plan-house-bold">
+      <div className="plan-logo">
+    <img src="https://www.brstore.us/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbrstore.5c796320.png&w=256&q=75" alt="Brstore" />
+     </div>
+        <h3>
+          Find the right <a className="pricing-text-plan">pricing</a> plan for
+          you
+        </h3>
+      </div>
+      <h5 className="pricing-description-plan">
+        Create a website, platform for your online users. We will help you every
+        step of the way. 🚀
+      </h5>
+      <div className="plan-house">
+        <div className="plans">
+          <Plan
+            name="Regular"
+            price="$1012.99/mo"
+            description="For building your first simple dream site"
+            features=" • Domain • Web Hosting • MySQL Database • SSL Certificate"
+          ></Plan>
+          <Plan
+            name="Premium"
+            price="$1032.99/mo"
+            description="For organization launches, frequently updated UI/UX, and more"
+            features=" • Domain • Web Hosting • MySQL Database • SSL Certificate • FTP Access • Unlimited Emails"
+          />
+          <Plan
+            name="Unlimited Access"
+            price="$1069.99/mo"
+            description="For Enterprise, directories, and commercial websites"
+            features="Domain • Web Hosting • MySQL Database • SSL Certificate • FTP Access • Unlimited Emails • Unlimited Access • User Support • VPS Server • Application Development"
+          />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default ChoosePlan;
